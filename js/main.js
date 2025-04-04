abe3f4765b1406636b196bca6837ffbba7dc8ea2
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-04-04 10:06:16 +0500

    Updated File Structure Ready

--- js/main.js
+++ js/main.js
@@ -1,17 +1,2 @@
 // js/pages/blog.js
-fetch('../data/blog.json')
-  .then(response => response.json())
-  .then(data => {
-    const blogContainer = document.querySelector('.blog-list');
-    data.forEach(post => {
-      const blogHTML = `
-        <div class="blog-card">
-          <img src="${post.image}" alt="${post.title}" />
-          <h3>${post.title}</h3>
-          <p>${post.excerpt}</p>
-          <small>By ${post.author} - ${post.date}</small>
-        </div>
-      `;
-      blogContainer.innerHTML += blogHTML;
-    });
-  });
+fetch('http://localhost:3000/Products').then((res)=> {res.json().then((data)=>{console.log(data)})}).catch((err)=>{console.log(err)})
